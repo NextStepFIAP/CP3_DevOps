4f9d195a6c77877ba41ba169223c30262540e047
Celso <61299686+Lorensatto99@users.noreply.github.com>
2021-11-01 21:39:44 -0300

    Update EpictaskApplication.java

--- src/main/java/br/com/fiap/epictask/EpictaskApplication.java
+++ src/main/java/br/com/fiap/epictask/EpictaskApplication.java
@@ -3,13 +3,20 @@ package br.com.fiap.epictask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
 @EnableSwagger2
-public class EpictaskApplication {
+public class EpictaskApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EpictaskApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpictaskApplication.class, args);
